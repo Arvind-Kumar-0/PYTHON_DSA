46d8b43a3e82dc0031495a7f0138bc087b0f813b
arvind <chintukadayan0@gmail.com>
2023-11-20 22:18:48 +0530

    add ll

--- LINKED_LIST.py
+++ LINKED_LIST.py
@@ -1,4 +1,6 @@
-array =[913, 578, 245, 754, 321, 987, 502, 689, 123, 876,
+
+
+array =[999, 578, 245, 754, 321, 987, 502, 689, 123, 876,
         450, 265, 831, 164, 729, 593, 316, 871, 104, 618,
         937, 782, 349, 516, 190, 847, 682, 415, 578, 236,
         691, 327, 864, 509, 147, 734, 602, 879, 394, 128,
@@ -18,7 +20,29 @@ class DoublyLinkedList(list):
         self.head = None
         self.tail = None
         self.length = 0
-    
+################################################################################################################################# 
+    def add_beg(self,val):
+        node = Node(val)
+        if self.length == 0:
+            self.head = node
+            self.tail = node
+            self.length += 1
+            return
+        self.head.last = node
+        node.next = self.head
+        self.head = node
+        self.length += 1
+################################################################################################################################# 
+    def del_beg(self):
+        self.head = self.head.next
+        self.head.last = None
+        self.length -= 1
+################################################################################################################################# 
+    def unappend(self):
+        self.tail = self.tail.last
+        self.tail.next = None
+        self.length -= 1
+#################################################################################################################################    
     def append(self,val):
         node = Node(val)
         if self.length == 0:
@@ -31,12 +55,14 @@ class DoublyLinkedList(list):
         self.tail = node
         self.length += 1
         return
-    
+#################################################################################################################################    
     def view(self):
         current = self.head
         while current != None:
-            print(" > ",current.val,end="")
+            print(current.val)
             current = current.next
+            
+##################################################################################################################################           
     def sort(self):
         sorter = []
         current = self.head
@@ -57,13 +83,54 @@ class DoublyLinkedList(list):
             current.val = sorter[index]
             current = current.next
             index += 1
+################################################################################################################################# 
     def __len__(self) -> int:
-        return self.length      
-            
-            
+        return self.length  
+#################################################################################################################################    
+    def insert(self, ind, val) -> None:
+        
+        if ind > self.length:
+            return Exception("Out of Index")
+        if ind == self.length:
+            return self.append(val)
+        if ind == 0:
+            return self.add_beg(val)
+        new = Node(val)
+        curr = self.head
+        i = 0
+        while i != ind-1:
+            curr = curr.next
+            i += 1
+        new.next = curr.next
+        new.last = curr
+        curr.next.last = new
+        curr.next = new
+        self.length += 1
+#################################################################################################################################                
+    def pop(self, ind = None) -> None:
+        if ind is None:
+            ind = self.length
+        if ind > self.length:
+            return Exception("Out of Index")
+        if ind == self.length:
+            return self.unappend()
+        if ind == 0:
+            return self.del_beg()
+        curr = self.head
+        i = 0
+        while i != ind:
+            curr = curr.next
+            i += 1      
+        curr.next.last = curr.last
+        curr.last.next = curr.next 
+        self.length -= 1 
+#################################################################################################################################  
+ 
 new = DoublyLinkedList()
 for i in array:
     new.append(i)
 new.sort()
+new.insert(0,12)
+new.pop(1)
 new.view()
 len(new)
